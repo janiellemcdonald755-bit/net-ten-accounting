d57bb136be81a6cc298b6d2e7a0775bcb5de648c
Janielle Mcdonald <janiellemcdonald755@gmail.com>
2026-04-11 10:44:01 -1200

    Fix import paths for App and index.css
    Updated import statements to match file naming conventions.

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App' // This must match your file name (App.jsx)
+import './index.css'   // Delete this line if you don't have an index.css file!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
