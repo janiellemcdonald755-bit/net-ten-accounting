a12ac2de558e23e08f02c528473150ebc721037c
Janielle Mcdonald <janiellemcdonald755@gmail.com>
2026-04-11 10:54:30 -1200

    Fix import statement for App component

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // This must match your file name (App.jsx)
+import App from './App.jsx' // This must match your file name (App.jsx)
 import './index.css'   // Delete this line if you don't have an index.css file!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
