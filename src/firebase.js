aa2941f2bda6712bdbaae9ddf6c34d7225b18ccf
Janielle Mcdonald <janiellemcdonald755@gmail.com>
2026-04-11 08:35:00 -1200

    Add Firebase initialization and configuration

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEpUog1zPTTSIG_oVX4o8ENZsDTO3krZE",
+  authDomain: "net-ten-accounting.firebaseapp.com",
+  projectId: "net-ten-accounting",
+  storageBucket: "net-ten-accounting.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "your-app-id"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
