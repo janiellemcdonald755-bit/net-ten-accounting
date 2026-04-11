6b924115c6fcda157dd9f95f2d66758cc821a8de
Janielle Mcdonald <janiellemcdonald755@gmail.com>
2026-04-11 10:17:47 -1200

    Update base path in Vite config for GitHub Pages
    Change base path for deployment on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures paths work on GitHub Pages
+  base: '/net-ten-accounting/', 
 })
